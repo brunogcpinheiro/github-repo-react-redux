73e87192df7e8ca1e64073763364e5436f799044
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-06-30 18:25:40 -0300

    adding favorites reducer, actions and view

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,18 +1,66 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-export default class Main extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
+
+class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string,
+    })).isRequired,
+  };
+
   state = {
     repositoryInput: '',
+  };
+
+  handleAddRepository = (e) => {
+    e.preventDefault();
+    this.props.addFavorite();
   };
 
   render() {
     return (
       <Fragment>
         <form onSubmit={this.handleAddRepository}>
-          <input placeholder="usuário/repositório" value={this.state.repositoryInput} />
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
           <button type="submit">Adicionar</button>
         </form>
+
+        <ul>
+          {this.props.favorites.map(favorite => (
+            <li key={favorite.key}>
+              <p>
+                <strong>{favorite.name}</strong> ({favorite.description})
+              </p>
+              <a href={favorite.url} target="blank">
+                Acessar
+              </a>
+            </li>
+          ))}
+        </ul>
       </Fragment>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
